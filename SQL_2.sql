cc360e0ab74f2ac0bd1a72cf8642993c7e389002
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-04-01 14:29:23 +0100

    nicnames for tables

--- SQL_2.sql
+++ SQL_2.sql
@@ -1,7 +1,14 @@
 -- customer id, name(first and last), mail adress;
 
-SELECT customer.customer_id, customer.last_name, customer.first_name;
-customer.adress
+SELECT customer.customer_id, customer.last_name,
+    address.address
 FROM customer, address
-Where customer.adress_id = adress.adress.id;
+Where customer.address_id = address.address_id;
 
+-- list of every film, category of that film, language of that film, 
+Select f.title, l.name, c.name
+From film f, film_category fc, language l , category c
+WHERE f.film_id = fc.film_id and c.category_id = fc.category_id and
+    l.language_id = f.language_id ; 
+
+-- nicknems for tables names - after table category use space and e.g first letter 
