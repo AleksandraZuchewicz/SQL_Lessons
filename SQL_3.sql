9afd9aeeb06e06eb4319971f5ad0a9f306925e47
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-04-01 14:53:13 +0100

    summary task 1,2

---
+++ SQL_3.sql
@@ -0,0 +1,5 @@
+-- how many times has each movie been rented out?
+SELECT f.title, count(r.rental_id)
+FROM film f, rental r, inventory i
+WHERE f.film_id = i.film_id and i.inventory_id = r.inventory_id
+GROUP BY 1; 
